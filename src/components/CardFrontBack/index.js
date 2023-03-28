98371302c3023bc63cd95c8d726aaeb1699b57e5
sswellington <sswellington@pm.me>
2023-03-28 20:42:09 -0300

    feat: add click event on card front back component
    `toggle` replace the following conditional structure:
    
    if ($cardFrontBack.classList.contains('-active')) {
        $cardFrontBack.classList.remove('-active');
    } else {
        $cardFrontBack.classList.add('-active');
    }
    
    substituted the .parentNode.parentNode.parentNode per `closest`

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -3,11 +3,19 @@ import './style.css';
 import CardGame from "../CardGame";
 
 const iconPath = "logo-html.png";
-const describe = "logo do html para carta";
+const describe = "logo do html para carta";  
 
 function CardFrontBack(icon = iconPath, alt = describe) {
+    window.cardFrontBack = {};
+    window.cardFrontBack.handleClick = (event) => {
+        const $origin = event.target;
+        const $cardFrontBack = $origin.closest('.card-front-back');
+
+        $cardFrontBack.classList.toggle('-active');
+    }      
+
     return /*html*/ `
-        <article class="card-front-back"> 
+        <article class="card-front-back" onClick="cardFrontBack.handleClick(event)"> 
             <div class="card -front">
                 ${CardGame()}
             </<div>
